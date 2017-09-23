1a065a1b5a52277a019e0d78c2b382cb36086c10
Lautaro López <lautaroml@MacBook-Pro-de-Lautaro.local>
2017-09-23 15:19:43 -0300

    Added es language

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contraseña debe tener al menos seis letras.',
+    'reset' => 'Se ha cambiado su contraseña satisfactoriamente!',
+    'sent' => 'Le hemos enviado un email para que cambie su contraseña!',
+    'token' => 'El token es inválido.',
+    'user' => "No hemos encontrado un usuario con ese e-mail.",
+
+];
