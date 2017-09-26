95570c45cdff0f650a7db6668b87a8e97e49d062
Lautaro López <lautaroml@MacBook-Pro-de-Lautaro.local>
2017-09-26 04:44:24 -0300

    foo

--- app/Taller.php
+++ app/Taller.php
@@ -10,4 +10,9 @@ class Taller extends Model
     {
         return $this->belongsToMany('App\User');
     }
+
+    public function compatibilities()
+    {
+        return $this->hasMany('App\Compatibility');
+    }
 }
