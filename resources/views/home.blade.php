c6788bdaae133a32615dfc9c407750b27a91f3e5
Lautaro López <lautaroml@MacBook-Pro-de-Lautaro.local>
2017-09-26 04:19:40 -0300

    foo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -35,17 +35,17 @@
                                         {{ $taller->professor }}
                                     </td>
                                     <td>
-                                        @if(in_array($taller->day_one, [6,7,8]))
+                                        @if(in_array($taller->day_one, [6,7,1]))
                                             X
                                         @endif
                                     </td>
                                     <td>
-                                        @if(in_array($taller->day_two, [6,7,8]))
+                                        @if(in_array($taller->day_two, [6,7,1]))
                                             X
                                         @endif
                                     </td>
                                     <td>
-                                        @if(in_array($taller->day_three, [6,7,8]))
+                                        @if(in_array($taller->day_three, [6,7,1]))
                                             X
                                         @endif
                                     </td>
