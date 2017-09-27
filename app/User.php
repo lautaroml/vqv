2e41b88993060c347a07690af1d4e5bb10771278
Lautaro López <lautaroml@MacBook-Pro-de-Lautaro.local>
2017-09-27 00:06:03 -0300

    foo

--- app/User.php
+++ app/User.php
@@ -15,7 +15,15 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'first_name',
+        'last_name',
+        'document',
+        'age',
+        'birthday',
+        'country_id',
+        'state_id',
+        'email',
+        'password',
     ];
 
     /**
