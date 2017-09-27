1d74d21ab93299298dda7ff6080aa60ac369ac21
Lautaro López <lautaroml@MacBook-Pro-de-Lautaro.local>
2017-09-26 23:56:01 -0300

    foo

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -71,7 +71,9 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'name' => $data['name'],
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
+            'document' => $data['last_name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
--- database/migrations/2017_09_27_025242_addColumnUserTable.php
+++ database/migrations/2017_09_27_025242_addColumnUserTable.php
@@ -13,7 +13,19 @@ class AddColumnUserTable extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('users', function($table)
+        {
+            $table->string('last_name');
+            $table->string('document');
+            $table->integer('age');
+            $table->date('birthday');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->unsignedInteger('country_id');
+            $table->unsignedInteger('state_id');
+            $table->unsignedInteger('city_id');
+            $table->string('elenco');
+        });
     }
 
     /**
@@ -23,6 +35,9 @@ class AddColumnUserTable extends Migration
      */
     public function down()
     {
-        //
+        /*Schema::table('users', function($table)
+        {
+            $table->dropColumn('new_col_name');
+        });*/
     }
 }
