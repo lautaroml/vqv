98e728d22ee5fd7076f6fd86707bd0c36f058c15
Lautaro López <lautaroml@MacBook-Pro-de-Lautaro.local>
2017-09-26 23:14:34 -0300

    Change email reset view

--- vendor/laravel/framework/src/Illuminate/Notifications/resources/views/email.blade.php
+++ vendor/laravel/framework/src/Illuminate/Notifications/resources/views/email.blade.php
@@ -6,7 +6,7 @@
 @if ($level == 'error')
 # Whoops!
 @else
-# Hello!
+# Hola!
 @endif
 @endif
 
@@ -45,14 +45,14 @@
 @if (! empty($salutation))
 {{ $salutation }}
 @else
-Regards,<br>{{ config('app.name') }}
+Saludos,<br>Vamos que venimos!
 @endif
 
 {{-- Subcopy --}}
 @isset($actionText)
 @component('mail::subcopy')
-If you’re having trouble clicking the "{{ $actionText }}" button, copy and paste the URL below
-into your web browser: [{{ $actionUrl }}]({{ $actionUrl }})
+Si tenes problemas al hacer click en el botón "{{ $actionText }}", copie y pegue la URL
+a continuación en su navegador web:: [{{ $actionUrl }}]({{ $actionUrl }})
 @endcomponent
 @endisset
 @endcomponent
