d50d669eb436a82f874b261a42911c1d1cd41784
Lautaro López <lautaroml@MacBook-Pro-de-Lautaro.local>
2017-09-26 23:37:53 -0300

    foo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -109,7 +109,7 @@
                             </div>
 
                             <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                                <label for="password" class="col-md-4 control-label">Password</label>
+                                <label for="password" class="col-md-4 control-label">Contraseña</label>
 
                                 <div class="col-md-6">
                                     <input id="password" type="password" class="form-control" name="password" required>
@@ -123,7 +123,7 @@
                             </div>
 
                             <div class="form-group">
-                                <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
+                                <label for="password-confirm" class="col-md-4 control-label">Confirmar contraseña</label>
 
                                 <div class="col-md-6">
                                     <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
