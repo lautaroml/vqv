a27a84feeb662970b78f8e11f2eafc8b0999f8d6
Lautaro López <lautaroml@hotmail.com>
2017-09-30 08:23:36 -0300

    Foo

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -181,10 +181,10 @@
 
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group pull-right">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Register
+                                    Confirmar
                                 </button>
                             </div>
                         </div>
