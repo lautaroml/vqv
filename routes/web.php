05a0e48e8a4be4699a7bd8dd1709bf8d06ad1032
Lautaro López <lautaroml@hotmail.com>
2017-09-30 10:58:01 -0300

    Fo

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,10 @@ Route::get('/', function () {
 });
 
 // TODO: Uncomment this.
-//Auth::routes()->middleware('up_date');
-//Route::get('/inscripcion', 'HomeController@index')->name('home')->middleware('up_date');
-//Route::get('/inscripcion/subscribe/{taller}', 'HomeController@subscribe')->name('subscribe')->middleware('up_date');
-//Route::get('/inscripcion/remove/{taller}', 'HomeController@remove')->name('remove')->middleware('up_date');
+Auth::routes()->middleware('up_date');
+Route::get('/inscripcion', 'HomeController@index')->name('home')->middleware('up_date');
+Route::get('/inscripcion/subscribe/{taller}', 'HomeController@subscribe')->name('subscribe')->middleware('up_date');
+Route::get('/inscripcion/remove/{taller}', 'HomeController@remove')->name('remove')->middleware('up_date');
 
 
 
