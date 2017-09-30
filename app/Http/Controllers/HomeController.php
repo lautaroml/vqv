dbcc703512d033a934e8ac011d34a37af239ca1e
Lautaro López <lautaroml@hotmail.com>
2017-09-30 11:46:03 -0300

    Foo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -56,7 +56,7 @@ class HomeController extends Controller
                 'user' => $user
             ], function ($message)  use ($taller, $user) {
                 $message->subject('Comprobante de inscripción al taller [' . $taller->name . ']');
-                $message->from('vamosquevenimosfestival@gmail.com');
+                $message->from('vamosquevenimosfestival@gmail.com', 'Vamos que venimos');
                 $message->to($user->email);
             });
 
