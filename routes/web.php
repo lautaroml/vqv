d4fc750765ac66ff0676ce293edcff7287ff4162
Lautaro López <lautaroml@hotmail.com>
2017-09-30 10:58:48 -0300

    Fo

--- routes/web.php
+++ routes/web.php
@@ -26,10 +26,10 @@ Route::get('/', function () {
 });
 
 // TODO: Uncomment this.
-Auth::routes()->middleware('up_date');
-Route::get('/inscripcion', 'HomeController@index')->name('home')->middleware('up_date');
-Route::get('/inscripcion/subscribe/{taller}', 'HomeController@subscribe')->name('subscribe')->middleware('up_date');
-Route::get('/inscripcion/remove/{taller}', 'HomeController@remove')->name('remove')->middleware('up_date');
+//Auth::routes()->middleware('up_date');
+//Route::get('/inscripcion', 'HomeController@index')->name('home')->middleware('up_date');
+//Route::get('/inscripcion/subscribe/{taller}', 'HomeController@subscribe')->name('subscribe')->middleware('up_date');
+//Route::get('/inscripcion/remove/{taller}', 'HomeController@remove')->name('remove')->middleware('up_date');
 
 
 
@@ -38,6 +38,7 @@ Route::get('/inscripcion/remove/{taller}', 'HomeController@remove')->name('remov
 Route::get('/prueba_mid', function () {
     $hoy = getdate();
     if ($hoy['mon'] == 9 && $hoy['mday'] == 30 && $hoy['hours'] >= 13) {
+        dd(1);
         if ( auth()->guest() ) {
             return view('auth.login');
         } else {
