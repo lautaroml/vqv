55b0f6231a5e7a832c633f9fb0f8583c4d980e47
Lautaro López <lautaroml@hotmail.com>
2018-06-13 20:54:51 -0300

    checkout commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'VQV') }}</title>
+    <title>VQV</title>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
